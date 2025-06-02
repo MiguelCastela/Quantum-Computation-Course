a3720b3c68719dbae97fa420c514d782844b8d52
MiguelCastela <125566095+MiguelCastela@users.noreply.github.com>
2025-06-02 01:43:50 +0100

    pe

--- Hybrid_conv_neural_network.py
+++ Hybrid_conv_neural_network.py
@@ -1,271 +1,130 @@
-import json
-import matplotlib.pyplot as plt
 import numpy as np
-from IPython.display import clear_output
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from skimage.transform import resize
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+
 from qiskit import QuantumCircuit
-from qiskit.circuit import ParameterVector
-from qiskit.circuit.library import ZFeatureMap
-from qiskit.quantum_info import SparsePauliOp
-from qiskit.primitives import StatevectorEstimator as Estimator
-from qiskit_machine_learning.optimizers import COBYLA
-from qiskit_machine_learning.utils import algorithm_globals
-from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
+from qiskit.circuit.library import ZZFeatureMap, TwoLocal
+from qiskit_machine_learning.connectors import TorchConnector
 from qiskit_machine_learning.neural_networks import EstimatorQNN
-from sklearn.model_selection import train_test_split
-import os
 
-# Create the directory if it doesn't exist
-output_dir = 'tutorial2'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+from torchvision import datasets, transforms
 
-# Set random seed for reproducibility
-algorithm_globals.random_seed = 9021
+# Set random seed
+torch.manual_seed(42)
 
-# Initialize the Estimator
-estimator = Estimator()
-
-# Define a two-qubit unitary circuit
-def conv_circuit(params):
-    target = QuantumCircuit(2)
-    target.rz(-np.pi / 2, 1)
-    target.cx(1, 0)
-    target.rz(params[0], 0)
-    target.ry(params[1], 1)
-    target.cx(0, 1)
-    target.ry(params[2], 1)
-    target.cx(1, 0)
-    target.rz(np.pi / 2, 0)
-    return target
-
-# Draw the convolutional circuit
-params = ParameterVector("θ", length=3)
-circuit = conv_circuit(params)
-circuit.draw("mpl", style="clifford")
-plt.savefig('tutorial2/convolutional_circuit.png')
-
-# Define a convolutional layer
-def conv_layer(num_qubits, param_prefix):
-    qc = QuantumCircuit(num_qubits, name="Convolutional Layer")
-    qubits = list(range(num_qubits))
-    param_index = 0
-    params = ParameterVector(param_prefix, length=num_qubits * 3)
-    for q1, q2 in zip(qubits[0::2], qubits[1::2]):
-        qc = qc.compose(conv_circuit(params[param_index : (param_index + 3)]), [q1, q2])
-        qc.barrier()
-        param_index += 3
-    for q1, q2 in zip(qubits[1::2], qubits[2::2] + [0]):
-        qc = qc.compose(conv_circuit(params[param_index : (param_index + 3)]), [q1, q2])
-        qc.barrier()
-        param_index += 3
-
-    qc_inst = qc.to_instruction()
-
-    qc = QuantumCircuit(num_qubits)
-    qc.append(qc_inst, qubits)
-    return qc
-
-# Draw the convolutional layer
-circuit = conv_layer(4, "θ")
-circuit.decompose().draw("mpl", style="clifford")
-plt.savefig('tutorial2/convolutional_layer.png')
-
-# Define a pooling circuit
-def pool_circuit(params):
-    target = QuantumCircuit(2)
-    target.rz(-np.pi / 2, 1)
-    target.cx(1, 0)
-    target.rz(params[0], 0)
-    target.ry(params[1], 1)
-    target.cx(0, 1)
-    target.ry(params[2], 1)
-    return target
-
-# Draw the pooling circuit
-params = ParameterVector("θ", length=3)
-circuit = pool_circuit(params)
-circuit.draw("mpl", style="clifford")
-plt.savefig('tutorial2/pooling_circuit.png')
-
-# Define a pooling layer
-def pool_layer(sources, sinks, param_prefix):
-    num_qubits = len(sources) + len(sinks)
-    qc = QuantumCircuit(num_qubits, name="Pooling Layer")
-    param_index = 0
-    params = ParameterVector(param_prefix, length=num_qubits // 2 * 3)
-    for source, sink in zip(sources, sinks):
-        qc = qc.compose(pool_circuit(params[param_index : (param_index + 3)]), [source, sink])
-        qc.barrier()
-        param_index += 3
-
-    qc_inst = qc.to_instruction()
-
-    qc = QuantumCircuit(num_qubits)
-    qc.append(qc_inst, range(num_qubits))
-    return qc
-
-# Draw the pooling layer
-sources = [0, 1]
-sinks = [2, 3]
-circuit = pool_layer(sources, sinks, "θ")
-circuit.decompose().draw("mpl", style="clifford")
-plt.savefig('tutorial2/pooling_layer.png')
-
-# Generate a dataset of images and labels
-def generate_dataset(num_images):
-    images = []
-    labels = []
-    hor_array = np.zeros((6, 8))
-    ver_array = np.zeros((4, 8))
-
-    j = 0
-    for i in range(0, 7):
-        if i != 3:
-            hor_array[j][i] = np.pi / 2
-            hor_array[j][i + 1] = np.pi / 2
-            j += 1
-
-    j = 0
-    for i in range(0, 4):
-        ver_array[j][i] = np.pi / 2
-        ver_array[j][i + 4] = np.pi / 2
-        j += 1
-
-    for n in range(num_images):
-        rng = algorithm_globals.random.integers(0, 2)
-        if rng == 0:
-            labels.append(-1)
-            random_image = algorithm_globals.random.integers(0, 6)
-            images.append(np.array(hor_array[random_image]))
-        elif rng == 1:
-            labels.append(1)
-            random_image = algorithm_globals.random.integers(0, 4)
-            images.append(np.array(ver_array[random_image]))
-
-        # Create noise
-        for i in range(8):
-            if images[-1][i] == 0:
-                images[-1][i] = algorithm_globals.random.uniform(0, np.pi / 16)
-    return images, labels
-
-# Generate a dataset with 10 images
-images, labels = generate_dataset(10)
-
-# Split the dataset into training and testing sets
-train_images, test_images, train_labels, test_labels = train_test_split(
-    images, labels, test_size=0.3, random_state=246
-)
-
-# Callback function to plot the objective function value against iteration
-def callback_graph(weights, obj_func_eval):
-    clear_output(wait=True)
-    objective_func_vals.append(obj_func_eval)
-    plt.title("Objective function value against iteration")
-    plt.xlabel("Iteration")
-    plt.ylabel("Objective function value")
-    plt.plot(range(len(objective_func_vals)), objective_func_vals)
-    plt.savefig('tutorial2/objective_function_value.png')
-    print(f"Iteration {len(objective_func_vals)} - Loss: {obj_func_eval}")
-
-# Visualize some training samples
-fig, ax = plt.subplots(2, 2, figsize=(10, 6), subplot_kw={"xticks": [], "yticks": []})
-for i in range(4):
-    ax[i // 2, i % 2].imshow(
-        train_images[i].reshape(2, 4),  # Change back to 2 by 4
-        aspect="equal",
+# -------------------------
+# 1. Load MNIST (0 and 1)
+# -------------------------
+def load_filtered_mnist(train=True, n_zeros=25, n_ones=25):
+    dataset = datasets.MNIST(
+        root="./data", train=train, download=True,
+        transform=transforms.Compose([transforms.ToTensor()])
     )
-plt.subplots_adjust(wspace=0.1, hspace=0.025)
-plt.savefig('tutorial2/training_samples.png')
+    targets = dataset.targets.numpy()
+    data = dataset.data.numpy()
 
-# Define the feature map
-feature_map = ZFeatureMap(8)
-feature_map.decompose().draw("mpl", style="clifford")
-plt.savefig('tutorial2/feature_map.png')
+    idx0 = np.where(targets == 0)[0][:n_zeros]
+    idx1 = np.where(targets == 1)[0][:n_ones]
+    idx = np.append(idx0, idx1)
 
-# Define the ansatz
-feature_map = ZFeatureMap(8)
-ansatz = QuantumCircuit(8, name="Ansatz")
+    data = data[idx]
+    labels = targets[idx]
+    images = data / 255.0
 
-# First Convolutional Layer
-ansatz.compose(conv_layer(8, "c1"), list(range(8)), inplace=True)
+    resized_images = np.array([resize(img, (2, 4), mode='constant').flatten() * np.pi for img in images])
+    labels = np.where(labels == 0, -1, 1)
 
-# First Pooling Layer
-ansatz.compose(pool_layer([0, 1, 2, 3], [4, 5, 6, 7], "p1"), list(range(8)), inplace=True)
+    return resized_images, labels
 
-# Second Convolutional Layer
-ansatz.compose(conv_layer(4, "c2"), list(range(4, 8)), inplace=True)
+train_images, train_labels = load_filtered_mnist(train=True, n_zeros=25, n_ones=25)
+test_images, test_labels = load_filtered_mnist(train=False, n_zeros=50, n_ones=50)
 
-# Second Pooling Layer
-ansatz.compose(pool_layer([0, 1], [2, 3], "p2"), list(range(4, 8)), inplace=True)
+# -------------------------
+# 2. Define Quantum Circuit
+# -------------------------
+num_qubits = 8
+feature_map = ZZFeatureMap(num_qubits)
+ansatz = TwoLocal(num_qubits, rotation_blocks="ry", entanglement_blocks="cz", reps=1)
 
-# Third Convolutional Layer
-ansatz.compose(conv_layer(2, "c3"), list(range(6, 8)), inplace=True)
+qc = QuantumCircuit(num_qubits)
+qc.compose(feature_map, inplace=True)
+qc.compose(ansatz, inplace=True)
 
-# Third Pooling Layer
-ansatz.compose(pool_layer([0], [1], "p3"), list(range(6, 8)), inplace=True)
-
-# Combine the feature map and ansatz
-circuit = QuantumCircuit(8)
-circuit.compose(feature_map, range(8), inplace=True)
-circuit.compose(ansatz, range(8), inplace=True)
-
-# Define the observable
-observable = SparsePauliOp.from_list([("Z" + "I" * 7, 1)])
-
-# Decompose the circuit for the QNN to avoid additional data copying
+# -------------------------
+# 3. Define Estimator QNN
+# -------------------------
 qnn = EstimatorQNN(
-    circuit=circuit.decompose(),
-    observables=observable,
+    circuit=qc,
     input_params=feature_map.parameters,
     weight_params=ansatz.parameters,
-    estimator=estimator,
 )
 
-# Draw the combined circuit
-circuit.draw("mpl", style="clifford")
-plt.savefig('tutorial2/combined_circuit.png')
+model = TorchConnector(qnn)
 
-# Initialize the classifier
-initial_point = np.random.random(qnn.num_weights)
-classifier = NeuralNetworkClassifier(
-    qnn,
-    optimizer=COBYLA(maxiter=200),  # Set max iterations here
-    callback=callback_graph,
-    initial_point=initial_point,
-)
+# -------------------------
+# 4. Build Hybrid Model
+# -------------------------
+class HybridClassifier(nn.Module):
+    def __init__(self, qnn):
+        super().__init__()
+        self.qnn = TorchConnector(qnn)
+    
+    def forward(self, x):
+        return self.qnn(x)
 
-# Convert training data to numpy arrays
-x = np.asarray(train_images)
-y = np.asarray(train_labels)
+hybrid_model = HybridClassifier(qnn)
 
-# List to store objective function values
-objective_func_vals = []
-plt.rcParams["figure.figsize"] = (12, 6)
+# -------------------------
+# 5. Train the Model
+# -------------------------
+def train(model, X, y, epochs=10, lr=0.1):
+    optimizer = Adam(model.parameters(), lr=lr)
+    loss_func = nn.MSELoss()
+    X_tensor = torch.tensor(X, dtype=torch.float32)
+    y_tensor = torch.tensor(y, dtype=torch.float32)
 
-# Train the classifier
-classifier.fit(x, y)
+    for epoch in range(epochs):
+        model.train()
+        optimizer.zero_grad()
+        output = model(X_tensor)
+        loss = loss_func(output.flatten(), y_tensor)
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {epoch+1:02d} | Loss: {loss.item():.4f}")
 
-# Print accuracy from the training data
-print(f"Accuracy from the train data : {np.round(100 * classifier.score(x, y), 2)}%")
+train(hybrid_model, train_images, train_labels)
 
-# Predict labels for the test images
-y_predict = classifier.predict(test_images)
+# -------------------------
+# 6. Evaluate the Model
+# -------------------------
+def evaluate(model, X, y_true):
+    model.eval()
+    with torch.no_grad():
+        X_tensor = torch.tensor(X, dtype=torch.float32)
+        predictions = model(X_tensor).flatten().numpy()
+        y_pred = np.where(predictions >= 0, 1, -1)
+    acc = accuracy_score(y_true, y_pred)
+    print(f"Accuracy: {acc:.2%}")
+    return y_pred
 
-# Convert test data to numpy arrays
-x = np.asarray(test_images)
-y = np.asarray(test_labels)
+y_pred = evaluate(hybrid_model, test_images, test_labels)
 
-# Print accuracy from the test data
-print(f"Accuracy from the test data : {np.round(100 * classifier.score(x, y), 2)}%")
-
-# Visualize some test samples and their predictions
-fig, ax = plt.subplots(2, 2, figsize=(10, 6), subplot_kw={"xticks": [], "yticks": []})
-for i in range(len(y_predict)):  # Use the length of y_predict to avoid index error
-    if y_predict[i] == -1:
-        ax[i // 2, i % 2].imshow(test_images[i].reshape(2, 4), aspect="equal")  # Reshaping appropriately
-        ax[i // 2, i % 2].set_title("The QCNN predicts this is a Horizontal Line")
-    if y_predict[i] == +1:
-        ax[i // 2, i % 2].imshow(test_images[i].reshape(4, 2), aspect="equal")  # Reshaping appropriately
-        ax[i // 2, i % 2].set_title("The QCNN predicts this is a Vertical Line")
-plt.subplots_adjust(wspace=0.1, hspace=0.5)
-plt.savefig('tutorial2/test_samples_predictions.png')
+# -------------------------
+# 7. Plot Results
+# -------------------------
+fig, ax = plt.subplots(2, 2, figsize=(6, 6))
+for i in range(4):
+    original_img = test_images[i].reshape((2, 4))
+    ax[i // 2, i % 2].imshow(original_img, cmap='gray')
+    label = "Digit 0" if y_pred[i] == -1 else "Digit 1"
+    ax[i // 2, i % 2].set_title(f"QCNN predicts: {label}")
+    ax[i // 2, i % 2].axis("off")
+plt.tight_layout()
+plt.show()
